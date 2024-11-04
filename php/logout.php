72a109255b6285b20edda1d77d8fa92badbcb64c
Mat Gonzalez <matiasgonzalez.652@gmail.com>
2024-11-04 19:01:22 -0300

    Se registra el acceso del usuario admin al sistema.

--- php/logout.php
+++ php/logout.php
@@ -6,13 +6,12 @@
 	$horarioDeEntrada = $_SESSION['horarioDeEntrada'];
 	$horarioDeSalida = date("Y-m-d H:i:s");
 	$usuario = $_SESSION['nombre'];
-	$cajainicial= $_SESSION['cajaInicial'];
-	if(is_null($cajainicial)){
-		$cajainicial=0;
-	}
+	$cajainicial = isset($_SESSION['cajaInicial']) ? $_SESSION['cajaInicial'] : 0;
 
-	if($_SESSION['rol'] == 2){
-		agregarUsuarioAPlanillaDeAsistenciaCantina($horarioDeEntrada,$horarioDeSalida,$usuario,$cajainicial);
+	if ($_SESSION['rol'] == 2) {
+		agregarUsuarioAPlanillaDeAsistenciaCantina($horarioDeEntrada, $horarioDeSalida, $usuario, $cajainicial);
+	} else if ($_SESSION['rol'] == 1) {
+		agregarAdministradorEnPlanilla($horarioDeEntrada, $horarioDeSalida, $usuario);
 	}
 	
 	session_unset();
@@ -20,6 +19,36 @@
 	session_destroy();
 
 	header('location: ../login.php');
+
+	function agregarAdministradorEnPlanilla($horarioDeEntrada, $horarioDeSalida, $usuario) {
+		include_once 'conexion.php';
+        $bd= new BD();
+        $conexion = $bd->connect();
+
+        $query= $conexion->prepare('INSERT INTO bdunertest.t_planilladeasistencias VALUES 
+							(null ,:nomyape, :horariodeentrada, :horariodesalida, :cajainicial, :cajafinal, :recaudado)');
+        
+        // Definir variables para los valores literales
+		$cajainicial = 0;
+		$cajafinal = 0;
+		$recaudado = 0;
+
+		// Vincular parámetros
+		$query->bindParam(':nomyape', $usuario);
+		$query->bindParam(':horariodeentrada', $horarioDeEntrada);
+		$query->bindParam(':horariodesalida', $horarioDeSalida);
+		$query->bindParam(':cajainicial', $cajainicial);
+		$query->bindParam(':cajafinal', $cajafinal);
+		$query->bindParam(':recaudado', $recaudado);
+
+        $query->execute();
+		
+		// Liberar la consulta y cerrar la conexión
+		$query->closeCursor();
+
+        $query = NULL;
+        $conexion = NULL;
+	}
 
 	function agregarUsuarioAPlanillaDeAsistenciaCantina($horarioDeEntrada,$horarioDeSalida,$usuario,$cajainicial){
 		include_once 'conexion.php';
