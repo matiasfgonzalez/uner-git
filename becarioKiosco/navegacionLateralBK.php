fd3b1c9b1e298f8dfb973743ee5c1e05b9a0fdf0
Mat Gonzalez <matiasgonzalez.652@gmail.com>
2024-11-04 19:16:43 -0300

    Formateo de codigo.

--- becarioKiosco/navegacionLateralBK.php
+++ becarioKiosco/navegacionLateralBK.php
@@ -1,53 +1,53 @@
 <!-- Sidebar -->
-    <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
-      <!-- Sidebar - Brand -->
-      <a class="sidebar-brand d-flex align-items-center justify-content-center" href="ventaBK.php">
-        <div class="sidebar-brand-icon rotate-n-15">
-          <!--<i class="fas fa-laugh-wink"></i>-->
-          <i class="fab fa-battle-net"></i>
-        </div>
-        <div class="sidebar-brand-text mx-3">CEFCA <sup>v1.0</sup></div>
-      </a>
+  <!-- Sidebar - Brand -->
+  <a class="sidebar-brand d-flex align-items-center justify-content-center" href="ventaBK.php">
+    <div class="sidebar-brand-icon rotate-n-15">
+      <!--<i class="fas fa-laugh-wink"></i>-->
+      <i class="fab fa-battle-net"></i>
+    </div>
+    <div class="sidebar-brand-text mx-3">CEFCA <sup>v1.0</sup></div>
+  </a>
 
-      <!-- Divider -->
-      <hr class="sidebar-divider my-0">
+  <!-- Divider -->
+  <hr class="sidebar-divider my-0">
 
-      <!-- Nav Item - Dashboard 
+  <!-- Nav Item - Dashboard 
       <li class="nav-item">
         <a class="nav-link" href="#">
           <i class="fas fa-newspaper"></i>
           <span>Noticias</span></a>
       </li>-->
 
-      <!-- Divider -->
-      <hr class="sidebar-divider">
+  <!-- Divider -->
+  <hr class="sidebar-divider">
 
-      <!-- Heading -->
-      <div class="sidebar-heading">
-        Productos
-      </div>
+  <!-- Heading -->
+  <div class="sidebar-heading">
+    Productos
+  </div>
 
-      <!-- Nav Item - Pages Collapse Menu -->
-      <li class="nav-item">
-        <a class="nav-link" href="ventaBK.php">
-          <i class="fas fa-cash-register"></i>
-          <span>Vender</span>
-        </a>
-        <a class="nav-link" href="agregarNuevoIncidenteCantinaBK.php">
-          <i class="fas fa-plus"></i>
-          <span>Agregar incidente</span>
-        </a>
-        <a class="nav-link" href="actualizarStockBK.php">
-          <i class="fas fa-truck-loading"></i>
-          <span>Actualizar producto</span>
-        </a>
-        <a class="nav-link" href="tablaVendidosHoyBK.php">
-          <i class="fas fa-cart-arrow-down"></i>
-          <span>Vendidos hoy</span>
-        </a>
-      </li>
-      <!-- Divider 
+  <!-- Nav Item - Pages Collapse Menu -->
+  <li class="nav-item">
+    <a class="nav-link" href="ventaBK.php">
+      <i class="fas fa-cash-register"></i>
+      <span>Vender</span>
+    </a>
+    <a class="nav-link" href="agregarNuevoIncidenteCantinaBK.php">
+      <i class="fas fa-plus"></i>
+      <span>Agregar incidente</span>
+    </a>
+    <a class="nav-link" href="actualizarStockBK.php">
+      <i class="fas fa-truck-loading"></i>
+      <span>Actualizar producto</span>
+    </a>
+    <a class="nav-link" href="tablaVendidosHoyBK.php">
+      <i class="fas fa-cart-arrow-down"></i>
+      <span>Vendidos hoy</span>
+    </a>
+  </li>
+  <!-- Divider 
       <hr class="sidebar-divider">
         <div class="sidebar-heading">
           Eventos
@@ -59,15 +59,15 @@
           </a>
         </li>-->
 
-      <!-- Divider -->
-      <hr class="sidebar-divider">
+  <!-- Divider -->
+  <hr class="sidebar-divider">
 
-      <!-- Heading 
+  <!-- Heading 
       <div class="sidebar-heading">
         Usuarios
       </div>-->
 
-      <!-- Nav Item - Pages Collapse Menu 
+  <!-- Nav Item - Pages Collapse Menu 
       <li class="nav-item">
         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true" aria-controls="collapsePages">
           <i class="fas fa-users"></i>
@@ -85,14 +85,14 @@
         </div>
       </li>-->
 
-      <!-- Nav Item - Charts 
+  <!-- Nav Item - Charts 
       <li class="nav-item">
         <a class="nav-link" href="reportes.php">
           <i class="fas fa-fw fa-chart-area"></i>
           <span>Reportes</span></a>
       </li>-->
 
-      <!-- Nav Item - Tables 
+  <!-- Nav Item - Tables 
       <li class="nav-item">
         <a class="nav-link" href="#">
           <i class="fas fa-fw fa-table"></i>
@@ -100,13 +100,13 @@
         </a>
       </li>-->
 
-      <!-- Divider 
+  <!-- Divider 
       <hr class="sidebar-divider d-none d-md-block">-->
 
-      <!-- Sidebar Toggler (Sidebar) -->
-      <div class="text-center d-none d-md-inline">
-        <button class="rounded-circle border-0" id="sidebarToggle"></button>
-      </div>
+  <!-- Sidebar Toggler (Sidebar) -->
+  <div class="text-center d-none d-md-inline">
+    <button class="rounded-circle border-0" id="sidebarToggle"></button>
+  </div>
 
-    </ul>
-    <!-- End of Sidebar -->
+</ul>
+<!-- End of Sidebar -->
